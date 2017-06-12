4ea3cf0abba3c055b0e4e9a1df3ae3f15727f421
Roberta Gardiman <robertagardiman@MacBook-Pro-di-Roberta-Gardiman.local>
2017-06-12 13:19:09 +0200

    added placeholder images in template and prev next logic

--- src/app/detail/detail.controller.js
+++ src/app/detail/detail.controller.js
@@ -7,12 +7,35 @@ export default class DetailController{
   }
   $onInit(){
     this.slug = this.state.params.slug;
-    this.item = this._projectsService.activeProject;
+    this.item = this._projectsService.activeProject; //current project
 
     if(!this.item){
       this._projectsService.getProjects().then((projects) => {
         this.item = projects.find(p => p.slug == this.slug);
       })
     }
+  }
+
+  next(item){
+    this._projectsService.getProjects().then((projects) => {
+      const currentProjectIndex = projects.findIndex(p => p.id == this.item.id);
+      console.log(currentProjectIndex);
+      let nextProject = projects[currentProjectIndex + 1];
+      if(!nextProject){
+        nextProject = projects[0];
+      }
+      this.state.go('Detail', {slug: nextProject.slug})
+    })
+  }
+
+  prev(item){
+    this._projectsService.getProjects().then((projects) => {
+      const currentProjectIndex = projects.findIndex(p => p.id == this.item.id);
+      let prevProject = projects[currentProjectIndex - 1];
+      if(!prevProject){
+        prevProject = projects[projects.length - 1];
+      }
+      this.state.go('Detail', {slug: prevProject.slug})
+    })
   }
 }
