f451f3d2e5976bf625bab450a788001b9a72a507
Roberta Gardiman <robertagardiman@MacBook-Pro-di-Roberta-Gardiman.local>
2017-06-19 16:13:11 +0200

    feat(portfolio): start working on style and added selected nav item

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,1 +1,15 @@
-export default class AppController {}
+export default class AppController {
+  /* @ngInject */
+  constructor($state){
+    this.state = $state;
+  }
+
+  setActive(pageName){
+    const urlPage = this.state.current.url;
+    if(urlPage && urlPage.indexOf(pageName) > -1){
+      return true;
+    }else {
+      return false;
+    }
+  }
+}
